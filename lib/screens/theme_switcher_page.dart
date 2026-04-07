95eb86ad2a5e177fea2cbb8d3bf950ea459aa783
sujit swain <sujitswain077@gmail.com>
2026-04-07 21:33:52 +0530

    enhance(ui): improve UX for theme switcher page

--- lib/screens/theme_switcher_page.dart
+++ lib/screens/theme_switcher_page.dart
@@ -115,7 +115,7 @@ class _ThemeSwitcherPageState extends State<ThemeSwitcherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0A0514),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
